6d43f39eabbd09b3221f88fca7585d648488e34a
minhwass <43117872+minhwass@users.noreply.github.com>
2022-08-09 16:58:00 +0900

    Time: 1 ms (98.27%), Space: 42.5 MB (93.66%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,28 +1,26 @@
 class Solution {
-   
-    List<List<Integer>> permutations = new ArrayList<>();
+    boolean [] used;
+    List<List<Integer>> perms = new ArrayList<>();
     
     public List<List<Integer>> permute(int[] nums) {
-        boolean [] used = new boolean[nums.length];
-        permute(new ArrayList<Integer>(), nums, used, 0);
-        return permutations;
+        used = new boolean[nums.length];
+        permute(nums, 0, new ArrayList<Integer>());
+        return perms;
     }
     
-    public void permute(ArrayList<Integer> perm, int [] nums,  boolean [] used, int count){
-        if(count == nums.length){
-            permutations.add(new ArrayList<>(perm));
-            return;
-        }
-        for(int i = 0 ; i < nums.length; i++){
-            if(!used[i]){
-                perm.add(nums[i]);
-                used[i] = true;
-                permute(perm, nums, used, count+1);
-                used[i] = false;
-                perm.remove(count);
+    public void permute(int [] nums, int count, ArrayList<Integer> list){
+        if(count >= nums.length){
+            perms.add(new ArrayList<>(list));
+        }else{
+            for(int i = 0 ; i < nums.length; i++){
+                if(!used[i]){
+                     used[i] = true;
+                    list.add(nums[i]);
+                    permute(nums, count + 1, list);
+                    list.remove(count);
+                    used[i] = false;
+                }
             }
         }
     }
-    
-    
 }
