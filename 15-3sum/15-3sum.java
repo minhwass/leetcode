7b7d1806bf2ec2a9df7e61785aba9dfd366f8454
minhwass <43117872+minhwass@users.noreply.github.com>
2022-08-12 00:31:48 +0900

    Time: 55 ms (31.24%), Space: 58.2 MB (80.87%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -7,6 +7,7 @@ class Solution {
         for(int i = 0 ; i < nums.length; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             int cur = nums[i];
+            if(cur > 0) break;
             int left = i+1; 
             int right = nums.length-1;
             int complement = 0 - nums[i];
