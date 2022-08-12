9255098a443fef32b00a73483ac60213ea554312
minhwass <43117872+minhwass@users.noreply.github.com>
2022-08-12 16:28:29 +0900

    Time: 554 ms (12.18%), Space: 68.4 MB (31.47%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int dest = nums.length-1;
+        boolean [] memoCanVisit = new boolean[nums.length];
+        Arrays.fill(memoCanVisit, true);
+        return canJump(nums, memoCanVisit, 0, dest);
+    }
+    
+    public boolean canJump(int [] nums, boolean [] memoCanVisit,  int start, int dest){
+        if(start == dest) return true;
+        if(!memoCanVisit[start]) return false;
+        if(start > dest || nums[start] == 0){
+            return memoCanVisit[start] = false;
+        } 
+        for(int i = 1; i <= nums[start]; i++){
+            if(canJump(nums, memoCanVisit, start+i, dest)) return true;
+        }
+        return memoCanVisit[start] = false;
+    }
+}
